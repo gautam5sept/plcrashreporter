b8318c95b7b786462118fe61887a0324ae60f3c0
Anna Kocheshkova <v-annkoc@microsoft.com>
2020-05-11 15:38:48 +0300

    Temporary version of XCF implementation

--- Scripts/build-tvos-universal.sh
+++ Scripts/build-tvos-universal.sh
@@ -2,6 +2,7 @@
 set -e
 
 # Helper variables
+WORK_DIR=build
 DEVICE_SDK="appletvos"
 SIMULATOR_SDK="appletvsimulator"
 TARGET_NAME="${PROJECT_NAME} tvOS Framework"
@@ -10,19 +11,27 @@ SIMULATOR_DIR="${BUILD_DIR}/${CONFIGURATION}-${SIMULATOR_SDK}"
 LIBRARY_BINARY="lib${PRODUCT_NAME}.a"
 FRAMEWORK_BINARY="${PRODUCT_NAME}.framework/${PRODUCT_NAME}"
 
+# The directory to gather all frameworks and build it into xcframework.
+XCFRAMEWORK_DIR="${WORK_DIR}/xcframework"
+
 # Building both SDKs
 build() {
     # Print only target name and issues. Mimic Xcode output to make prettify tools happy.
     echo "=== BUILD TARGET $1 OF PROJECT ${PROJECT_NAME} WITH CONFIGURATION ${CONFIGURATION} ==="
     # OBJROOT must be customized to avoid conflicts with the current process.
     xcodebuild -quiet \
-        SYMROOT="${SYMROOT}" OBJROOT="${BUILT_PRODUCTS_DIR}" PROJECT_TEMP_DIR="${PROJECT_TEMP_DIR}" \
+    PROJECT_TEMP_DIR="${PROJECT_TEMP_DIR}" \
         ONLY_ACTIVE_ARCH=NO BITCODE_GENERATION_MODE=bitcode OTHER_CFLAGS="-fembed-bitcode" \
-        -project "${PROJECT_NAME}.xcodeproj" -configuration "${CONFIGURATION}" -target "$1" -sdk "$2"
+        -project "${PROJECT_NAME}.xcodeproj" -configuration "Release" -target "$1" -sdk "$2"
 }
 echo "Building the library for ${DEVICE_SDK} and ${SIMULATOR_SDK}..."
 build "${TARGET_NAME}" "${DEVICE_SDK}"
 build "${TARGET_NAME}" "${SIMULATOR_SDK}"
+
+# Copy all framework files to use them for xcframework file creation.
+mkdir -p "${XCFRAMEWORK_DIR}"
+cp -R "${WORK_DIR}/Release-${DEVICE_SDK}/" "${XCFRAMEWORK_DIR}/Release-${DEVICE_SDK}"
+cp -R "${WORK_DIR}/Release-${SIMULATOR_SDK}/" "${XCFRAMEWORK_DIR}/Release-${SIMULATOR_SDK}"
 
 # Clean output folder
 rm -rf "${BUILT_PRODUCTS_DIR}"
---
+++ Scripts/build-xcframework.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+# Work dir is directory where all XCFramework artifacts is stored.
+WORK_DIR="${SRCROOT}/build"
+XCFRAMEWORK_DIR="${WORK_DIR}/xcframework"
+MACOSX_DIR="MacOSX"
+
+# Work dir will be the final output to the framework.
+XC_FRAMEWORK_PATH="${XCFRAMEWORK_DIR}/Output/${PROJECT_NAME}.xcframework"
+
+# Additionally copy macos files.
+cp -R "${WORK_DIR}/Release-${MACOSX_DIR}/" "${XCFRAMEWORK_DIR}/Release-${MACOSX_DIR}"
+
+# Clean previus XCFramework build.
+rm -rf ${PROJECT_NAME}.xcframework/
+
+# Build XCFramework.
+function SetXcBuildCommandFramework() {
+    FRAMEWORK_PATH="$XCFRAMEWORK_DIR/Release-$1/${PROJECT_NAME}.framework"
+    echo $FRAMEWORK_PATH
+    [ -e "$FRAMEWORK_PATH" ] && XC_BUILD_COMMAND="$XC_BUILD_COMMAND -framework $FRAMEWORK_PATH";
+}
+
+# Create a cycle instead next lines
+SetXcBuildCommandFramework "iphoneos"
+SetXcBuildCommandFramework "iphonesimulator"
+SetXcBuildCommandFramework "appletvos"
+SetXcBuildCommandFramework "appletvsimulator"
+SetXcBuildCommandFramework "MacOSX"
+
+XC_BUILD_COMMAND="xcodebuild -create-xcframework $XC_BUILD_COMMAND -output $XC_FRAMEWORK_PATH"
+eval "$XC_BUILD_COMMAND"
